8a623aeb05d5ac0c93d3f199f07616468e829b6b
ymkim <ymkim@enliple.com>
2020-03-20 16:20:07 +0900

    swing search program start!!

---
+++ src/main/java/com/bymin/swing/SwingApplication.java
@@ -0,0 +1,13 @@
+package com.bymin.swing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bymin/swing/SwingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bymin.swing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
